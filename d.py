6d7576d46af1b8ca9714a8531980acb203cccd58
deleteInteger <55137104+deleteInteger@users.noreply.github.com>
2019-10-04 14:20:06 +0800

    Delete d.py

--- d.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1', endpoint_url="http://192.168.87.127:8000")
-
-table = dynamodb.Table('TibameTable')
-
-table.delete()
